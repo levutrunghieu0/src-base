08020a0b60c59411b06e48a31c0b7a43c1b2e845
HIEU\hieul <hieuletrung2208@gmail.com>
2023-09-18 00:12:27 +0700

    test

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -4,6 +4,6 @@ import { Sequelize } from 'sequelize-typescript';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! hehehe';
   }
 }
